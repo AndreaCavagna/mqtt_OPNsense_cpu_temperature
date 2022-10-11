2e58968aaabf3c5e143d578227360f42386413cc
Andrea Cavagna <andrea.cavagna.94@gmail.com>
2022-10-11 22:58:26 +0200

    Minor changes
    Set default number of cores to 1
    Delay between core readings

--- helpers/MeasurementType.py
+++ helpers/MeasurementType.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import os
+import time
 
 MeasurementType = Enum('MeasurementType', 'AVERAGE MAXIMUM')
 
@@ -8,6 +9,7 @@ def averageMeasurament(N_CORES: int):
   cpu_temp = 0
   for i in range(N_CORES):
     cpu_temp = float(os.popen('sysctl dev.cpu.'+ str(i)+'.temperature').read()[23:27])
+    time.sleep(0.5)
   return cpu_temp/ float(N_CORES) 
 
 
@@ -15,6 +17,7 @@ def MaxMeasurament(N_CORES: int):
   max_temp = 0  
   for i in range(N_CORES):
     cpu_temp = float(os.popen('sysctl dev.cpu.'+ str(i)+'.temperature').read()[23:27])
+    time.sleep(0.5)
     if cpu_temp > max_temp:
         max_temp = cpu_temp
   return max_temp
